51562328fd514a17d2f01cf33330dd8e811cce39
justin-poulose <justinchekkan@gmail.com>
2024-02-01 18:08:16 -0500

    created all pages functions working

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
-
+const axios = require('axios');
 
 const app = express();
+
+let post = [];
+const dbURL= "http://localhost:8000/posts";
+const fetchData = async () => {
+    try {
+        const response = await axios.get(dbURL); // Make GET request to dbURL
+        post = response.data; // Store data in posts variable
+        console.log("Data fetched successfully");
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+};
+fetchData();
 
 app.set('view engine','ejs');
 
@@ -9,12 +22,38 @@ app.listen(3000);
 
 app.use(express.static('public'));
 app.get('/',(req,res)=>{
-    res.render('index');
+    fetchData();
+    res.render('index',{ title: 'Home', post });
 
 });
 app.get('/createpost',(req,res)=>{
-    res.render('create');
+    res.render('create',{ title: 'create'});
 });
+app.get('/sucessful',(req,res)=>{
+    res.render('sucessful',{ title: 'sucessful'});
+});
+app.get('/posts/:postId', async (req, res) => {
+    const postId = req.params.postId;
+
+    try {
+        const response = await axios.get(`${dbURL}/${postId}`);
+        const postDetails = response.data;
+        res.render('postDetails', { postDetails, title: 'post'});
+    } catch (error) {
+        console.error("Error fetching post details:", error);
+        res.status(500).send("Error fetching post details");
+    }
+});
+
+app.delete('/posts/:postId', (req, res) => {
+    const postId = req.params.postId;
+    fetch('http://localhost:8000/posts/' + postId, {
+            method: 'DELETE',});
+    res.sendStatus(204); 
+});
+
+
+
 app.use((req,res)=>{
-    res.status(404).render('404');
+    res.status(404).render('404',{ title: '404'});
 })
